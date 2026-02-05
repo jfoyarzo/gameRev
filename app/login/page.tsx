2e465f92e17697c8da622df775e631d886e20f14
jfoyarzo <foyarzo621@gmail.com>
2026-02-04 21:09:16 -0300

    Feat: Update sign in page with link to signup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/components/auth/login-form"
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
+import Link from "next/link"
 
 export default async function LoginPage() {
     const session = await auth()
@@ -16,6 +17,12 @@ export default async function LoginPage() {
                     Sign in to access your account
                 </p>
                 <LoginForm />
+                <p className="text-sm text-center text-gray-500">
+                    Don&apos;t have an account?{' '}
+                    <Link href="/signup" className="text-primary hover:underline font-medium">
+                        Sign up
+                    </Link>
+                </p>
             </div>
         </div>
     )
